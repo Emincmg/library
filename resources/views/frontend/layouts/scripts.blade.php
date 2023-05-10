b262e7ddc905251e1f74cbf1ca836106051c8bfa
Emincmg <emin-comoglu@hotmail.com>
2023-05-11 00:24:25 +0300

    Featured book page improvements

--- resources/views/frontend/layouts/scripts.blade.php
+++ resources/views/frontend/layouts/scripts.blade.php
@@ -57,11 +57,14 @@
 
         //View featured book modal
         $(document).on('click', '#featuredBook', function (e) {
-            //TODO : Null safety
+            let bookID = {{$featuredBook->id}};
+            let book = booksdata.find(book => book.id === bookID);
+
             $('#viewModal').modal('show');
-            $('#book_dtl_title').text('{{$featuredBook->book_title}}');
-            $('#book_dtl_explanation').text('{{$featuredBook->book_explanation}}');
-            $('#book_dtl_img').attr("src", '{{$featuredBook->book_img}}');
+            $('#book_dtl_title').text(book.book_title + " - " + book.book_author);
+            $('#book_dtl_explanation').text(book.book_explanation);
+            $('#book_dtl_category_date').text("Categories : " + book.book_category + " | " + "Published : " + book.book_date);
+            $('#book_dtl_img').attr('src', book.book_img);
         });
 
         //Delete clicked book from server
