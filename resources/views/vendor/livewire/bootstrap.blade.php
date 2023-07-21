4a475e3409e9bca5b302539db0377c100f2b4a50
Emincmg <emin-comoglu@hotmail.com>
2023-07-21 12:52:08 +0300

    New theme

--- resources/views/vendor/livewire/bootstrap.blade.php
+++ resources/views/vendor/livewire/bootstrap.blade.php
@@ -17,7 +17,6 @@
 
                 {{-- Pagination Elements --}}
                 @foreach ($elements as $element)
-                    {{-- "Three Dots" Separator --}}
                     @if (is_string($element))
                         <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
                     @endif
