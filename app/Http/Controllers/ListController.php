d455f9a38b9b3477b761dc072f4bc47c078efd1b
Emincmg <emin-comoglu@hotmail.com>
2023-08-23 19:57:39 +0300

    Code reorganized

--- app/Http/Controllers/ListController.php
+++ app/Http/Controllers/ListController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 use Symfony\Component\HttpFoundation\Response;
 
 class ListController extends Controller
 {
-    public function index()
+    public function index() : View
     {
         $user = Auth::user();
-        
+
         $readBooks = $user->books()->where('readBefore', true)->get();
         $unreadBooks = $user->books()->where('readBefore', false)->get();
 
