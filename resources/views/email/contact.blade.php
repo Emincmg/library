8cf6fba9f2289fb5218b85796d00ec8900f7fb5b
Emincmg <emin-comoglu@hotmail.com>
2023-10-10 23:32:04 +0300

    Code reorganized

---
+++ resources/views/email/contact.blade.php
@@ -0,0 +1,15 @@
+<h2>{{ $mailData->name }}</h2>
+
+<br>
+
+
+
+<strong>Details: </strong><br>
+
+<strong>Name: </strong>{{ $mailData->name }} <br>
+
+<strong>Email: </strong>{{ $mailData->email }} <br>
+
+
+
+Thank you
